4d4f882f20101b0f62b2147f0469d81fe6eed18e
bpull <brandonbsp@gmail.com>
2016-09-06 23:57:19 -0400

    working on ai proj1

--- ai/proj/1/connect4.py
+++ ai/proj/1/connect4.py
@@ -5,28 +5,26 @@ import numpy as np
 class Board:
 
     def __init__(self):
-        self.b = np.ndarray(size=(7,6), dtype=str)
+        self.b = np.ndarray(shape=(6,7), dtype=(str,1))
         self.b.fill('*')
 
     def __repr__(self):
-        for i in range(7):
-            for j in range(6):
+        for i in range(6):
+            for j in range(7):
                 print(str(self.b[i][j]), end="")
             print('')
-        print ("1234567\n")
+        print ("1234567")
+        return ""
 
     def play_piece(self, column, token):
-        for i in range(6):
-            if self.b[column][i] == '*':
-                self.b[column][i] = str(token)
-                return true
-        return false
-
-class Player:
-
-    def __init__(self, color):
-        self.color = color
-        self.win = False
+        for i in range(5,-1,-1):
+            try:
+                if self.b[i][column] == '*':
+                    self.b[i][column] = str(token)
+                    return i
+            except Exception as e:
+                return False
+        return False
 
 
 
@@ -34,17 +32,46 @@ class Game:
 
     def __init__(self):
         self.b = Board()
-        self.red = Player('R')
-        self.black = Player('B')
-        self.turn = 'R'
+        self.turn = 'B'
+        self.input = ''
+        self.over = False
 
     def next_move(self):
-        if self.turn == 'R':
-            print ("Red player, what's your move?\n")
-        else:
-            print ("Black player, what's your move?\n")
+        print (self.b)
 
-    def play_turn(self, column):
-        '''start working here you twat'''
+        if self.turn == 'R':
+            print ("Red player, what's your move?")
+        else:
+            print ("Black player, what's your move?")
+        self.input = input()
+        if self.input.isdigit():
+            self.input = int(self.input) - 1
+
+    def play_turn(self):
+        outcome = self.b.play_piece(self.input, self.turn)
+        if outcome is not False:
+            self.check_win(outcome)
+        else:
+            print ("Invalid move! Please try again!")
+            self.next_move()
+            self.play_turn()
+
+    def check_win(self, row):
+        return
+
+    def change_player(self):
+        if self.turn is 'B':
+            self.turn = 'R'
+        else:
+            self.turn = 'B'
+
+
+
 
 if __name__ == '__main__':
+    curGame = Game()
+
+    while curGame.over is not True:
+        curGame.change_player()
+        curGame.next_move()
+        curGame.play_turn()
