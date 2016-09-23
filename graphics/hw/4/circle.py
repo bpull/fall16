c0ff42cb472cdb52026fcf186af541c9bf246fb4
bpull <brandonbsp@gmail.com>
2016-09-22 20:35:13 -0400

    graphics hw 4

---
+++ graphics/hw/4/circle.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+
+# Our required libraries!
+import math
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from OpenGL.GLUT import *
+
+def createCircle(cx, cy, rad, verts):
+    theta = 360/verts
+    count = 1
+
+    while theta <= 360:
+        theta = theta * count
+        x = (cx + rad * math.cos(theta))
+        y = (cy + rad * math.sin(theta))
+        glVertex3f(x, y, 0.0)
+        count = count + 1
+
+
+# The function that we will use to draw the environment
+def display():
+    verts = 'a'
+    while not verts.isdigit():
+        verts = input("How many vertices for your circle?")
+        verts = int(verts)
+
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+    glColor3f(1.0, 1.0, 1.0)   # White
+
+    glBegin(GL_LINE_LOOP)
+    createCircle(50, 50, 25, verts)
+    glEnd()
+
+    glFlush()
+
+# Initialize the environment
+glutInit(sys.argv)
+glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB|GLUT_DEPTH)
+
+# Set the initial window position and size (if we want to)
+glutInitWindowPosition(100,50)
+glutInitWindowSize(400, 400)
+
+# Create the window and name it
+glutCreateWindow("Brandon Pullig 175148")
+
+# Build the scene
+glutDisplayFunc(display)
+
+# Build the Bounding box - much more on this later!
+glOrtho(0.0, 100.0, 0.0, 100.0, -2.0, 1.0)
+
+# Go into a loop
+glutMainLoop()
